f94a09620d1615343323d7bbe7da1ced1292a983
Shing Hei Chan (Tommy) <chanhei888@gmail.com>
2025-03-28 14:06:45 +0000

    delete unnecessary file

--- backend/db.py
+++
@@ -1,9 +0,0 @@
-import os
-from supabase import create_client, Client
-from dotenv import load_dotenv
-
-load_dotenv()
-
-supabase_url: str = os.environ['SUPABASE_URL']
-supabase_key: str = os.environ['SUPABASE_KEY']
-supabase: Client = create_client(supabase_url, supabase_key)
